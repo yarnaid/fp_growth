d6e02c525e8fefaafdd87858d719f06c6cd5277d
yarnaid <yarnaid@gmail.com>
2015-03-03 16:03:08 +0300

    fp-growth adoptation

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
-all:
-	g++ main.cpp -std=c++11 -o main
+all: fp_growth
+	g++ main.cpp ./FP-growth/fptree.o -std=c++11 -o main
+
+fp_growth:
+	g++ -g -c FP-growth/fptree.cpp -std=c++11 -o FP-growth/fptree.o
--- main.cpp
+++ main.cpp
@@ -1,7 +1,48 @@
+#include <iostream>
+#include <vector>
+
 #include "FP-growth/fptree.hpp"
+
+
+void test_fp()
+{
+    /* code */
+    const Item a{1};
+    const Item b{2};
+    const Item c{3};
+    const Item d{4};
+    const Item e{5};
+    const Item f{6};
+
+    // const Item a{ "A" };
+    // const Item b{ "B" };
+    // const Item c{ "C" };
+    // const Item d{ "D" };
+    // const Item e{ "E" };
+
+    const std::vector<Transaction> transactions{
+        { a, b },
+        { b, c, d },
+        { a, c, d, e },
+        { a, d, e },
+        { a, b, c },
+        { a, b, c, d },
+        { a },
+        { a, b, c },
+        { a, b, d },
+        { b, c, e }
+    };
+
+    const unsigned minimum_support_treshold = 2;
+
+    const FPTree fptree{ transactions, minimum_support_treshold };
+
+    const std::set<Pattern> patterns = fptree_growth( fptree );
+}
 
 int main(int argc, char const *argv[])
 {
     /* code */
+    test_fp();
     return 0;
 }
